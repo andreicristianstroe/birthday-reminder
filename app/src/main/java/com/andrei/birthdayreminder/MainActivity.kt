fe293dd092658207513b7ee7c86d554d0819d1ba
Andrei-Cristian <andrei.stroe@protonmail.com>
2024-05-30 00:26:00 +0300

    small fixes

--- app/src/main/java/com/andrei/birthdayreminder/MainActivity.kt
+++ app/src/main/java/com/andrei/birthdayreminder/MainActivity.kt
@@ -50,7 +50,9 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    @Deprecated("This method has been deprecated in favor of using the\n      {@link OnBackPressedDispatcher} via {@link #getOnBackPressedDispatcher()}.\n      The OnBackPressedDispatcher controls how back button events are dispatched\n      to one or more {@link OnBackPressedCallback} objects.")
     override fun onBackPressed() {
+        @Suppress("DEPRECATION")
         super.onBackPressed()
         if (adapter.contactList == 0) {
             adapter = ContactsAdapter(this, null, listOfMonths())
